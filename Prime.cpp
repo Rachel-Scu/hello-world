374f50ee0cfc037883c5ef0b27cefdd604e89e5a
Cuijiaqi <55613873+Rachel-Scu@users.noreply.github.com>
2020-03-13 13:35:51 +0800

    Update Prime.cpp
    edtion-1

--- Prime.cpp
+++ Prime.cpp
@@ -1,3 +1,6 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
 class Prime {
 public:
   Prime(int num);
@@ -11,13 +14,49 @@ public:
 private:
   const int mNum;
 };
+Prime::Prime(int num):mNum(num){}
+Prime::~Prime(){}
+Prime Prime::sumBit(){
+	int Num=mNum,sum=0;
+	while(Num){
+		sum+=Num%10;
+		Num/=10;
+	}
+  	return sum;
+}
+Prime Prime::sqaureSumBit(){
+	int Num=mNum,sum=0;
+	while(Num){
+		sum+=pow(Num%10,2);
+		Num/=10;
+	}
+  	return sum;
+}
+Prime Prime::multiBit(){
+	int Num=mNum,sum=0;
+	while(Num){
+		sum*=(Num%10);
+		Num/=10;
+	}
+  	return sum;
+}
+bool Prime::isPrime(){
+	int Num=mNum; 
+	for(int i=2;i<Num;i++){
+		if(Num%i==0){
+			return false;
+		}
+	} 
+  	return true;
+}
 
 int main() {
   Prime pm(113);
   Prime sb = pm.sumBit();
   Prime mb = pm.multiBit();
   Prime ssb = pm.sqaureSumBit();
-  if(pm.isPrime() && sb.isPrime() && mb.isPrime() && ssb.isPrime())
-    ; //to do sth
+  if(pm.isPrime() && sb.isPrime() && mb.isPrime() && ssb.isPrime()){
+  	cout<<"113 is super prime."<<endl;
+  }//to do sth
   return 0;
 }
