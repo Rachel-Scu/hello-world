635d521e733eebd4c35646d0eee48401f70308c6
Cuijiaqi <55613873+Rachel-Scu@users.noreply.github.com>
2020-03-20 11:02:20 +0800

    Update struct-ori.cpp
    add constructions.

--- struct-ori.cpp
+++ struct-ori.cpp
@@ -5,18 +5,16 @@ class DateTime{
 		int year,month,day;
 		int hour,minute,second;
 	public:
-		void inTime();
+		DateTime();
 		void showTime();    //static 只能在该.cpp中使用 
 }; 
 int main(){
 	DateTime dt,dt1;
-	dt.inTime();
 	dt.showTime();
-	dt1.inTime();
 	dt1.showTime();  
 	return 0;
 }
-void DateTime::inTime(){
+DateTime::DateTime(){
 	year=2020;month=3;day=13;
 	hour=10;minute=25;second=55;
 }
