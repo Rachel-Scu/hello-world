3a36b261ba8407b2c3a30fb495a86f95b5383d48
Cuijiaqi <55613873+Rachel-Scu@users.noreply.github.com>
2020-03-21 16:44:53 +0800

    Update struct-ori.cpp

--- struct-ori.cpp
+++ struct-ori.cpp
@@ -6,17 +6,28 @@ class DateTime{
 		int hour,minute,second;
 	public:
 		DateTime();
-		void showTime();    //static 只能在该.cpp中使用 
+		~DateTime();
+		void showTime(); 
+		void showMoon();   
 }; 
 int main(){
 	DateTime dt,dt1;
 	dt.showTime();
-	dt1.showTime();  
+	dt1.showMoon();  
 	return 0;
 }
 DateTime::DateTime(){
-	year=2020;month=3;day=13;
-	hour=10;minute=25;second=55;
+	cout<<this<<endl;
+	year=2020;month=3;day=20;
+	hour=11;minute=46;second=55;
+}
+DateTime::~DateTime(){
+	cout<<this<<"Desrtuctor called"<<endl;
+}
+void DateTime::showMoon(){
+	year=2020;month=2;day=27;
+	hour=11;minute=46;second=55;
+	showTime();
 }
 void DateTime::showTime(){
 	printf("当前时间：%d/%d/%d %d:%d:%d\n",year,month,day,hour,minute,second);
