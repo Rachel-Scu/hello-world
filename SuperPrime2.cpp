3789e8fca40f3c88ae1fbf270b32afe4b5caa81c
Cuijiaqi <55613873+Rachel-Scu@users.noreply.github.com>
2020-05-14 15:30:43 +0800

    accomplished homew

--- SuperPrime2.cpp
+++ SuperPrime2.cpp
@@ -1,104 +1,59 @@
-//作业：面向对象设计以下框架的代码细节，程序能编译运行得到正确结果 
+//作业：删除多余的集合类，使用继承方法实现相同的功能。 
 #include <iostream>
-class SuperPrime {
+class Prime {
   public:
-  	SuperPrime():number(0) {  //为什么必须有？ 
-  	  size = 0;
-  	}
-  	SuperPrime(int n):number(n) {
-  	  size = 0;
-  	  split();  //它就是构造对象 
+  	Prime():number(0) {
 	}
-//  	~SuperPrime() {
-//  	  for (int i = 0; i < size; ++i)  //销毁对象 
-//		delete N[i]; 
-//	}
-  	bool isSuperPrime() {
-  	  SuperPrime a(sum());   //将普通整数转变为对象 
-	  SuperPrime b(multi());
-	  SuperPrime c(squareSum());
-	  if (isPrime() && a.isPrime() && b.isPrime() && c.isPrime())
+  	Prime(int n):number(n) {
+	}
+	~Prime() {
+	}
+  	bool isPrime() { 
+  	  //2到number-1的因子 
+  	  for(int i=2;i<number;i++){
+  	  	if(number%i==0)
+  	  		return false;
+	  } 
+  	  return true;
+	}
+  private:
+  	const int number;
+}; 
+
+class SuperPrime : public Prime {
+  public:
+  	SuperPrime():Prime(0){  
+  	}
+  	SuperPrime(int n):Prime(n){
+	  // number split into N
+	  int temp = n;
+	  int tsum=0,tmulti=1,tsquaresum=0;
+	  while(temp > 0) {
+	  	int t = temp % 10;
+	  	temp /= 10;
+	  	tsum+=t;
+		tmulti*=t;
+		tsquaresum+=(t*t);
+		psum=new Prime(tsum);
+		pmulti=new Prime(tmulti);
+		psquaresum=new Prime(tsquaresum);   //作业：单个数字为对象？还是和/积/平方和为对象？ 
+	  } 
+	}
+  	~SuperPrime() {
+	}
+  	bool isPrime() {   //类/对象的接口，更抽象说是外观 
+	  if (Prime::isPrime() && psum->isPrime() && pmulti->isPrime() && psquaresum->isPrime())
 	    return true; 
   	  return false;
 	}
   private:
-  	const int number;
-  	SuperPrime *N[100];
-	int size;
-  	bool isPrime() { 
-  	  //2到number-1的因子 
-  	  for(int i = 2; i < number; i++){
-        if (number % i == 0) 
-            return false;
-      }
-    	return true;
-	}
-	void split() {   //工厂方法设计模式 
-	  // number split into N
-	  if(number<10){
-	  	size=1;
-	  	return;
-	  } 
-	  int temp = number;
-	  while(temp > 0) {
-	  	int n = temp % 10;
-	  	temp /= 10;
-	  	N[size] = new SuperPrime(n);   //构造对象 
-	  	size += 1;
-	  } 
-	}
-	int sum() {
-	  int s=0;
-	  for(int i=0;i<size;i++){
-	  	s+=N[i]->number;
-	  }	
-	  return s;
-	}
-	int multi() {
-	  int mul=1;
-	  for(int i=0;i<size;i++){
-	  	mul*=N[i]->number;
-	  }
-	  return mul;	
-	}
-	int squareSum() {
-	  int ss=0;
-	  for(int i=0;i<size;i++){
-	  	ss=ss+N[i]->number*N[i]->number;
-	  }
-	  return ss;	
-	}
-};
-class Set {
-  public:
-  	Set(int from, int to) {
-  	  size = 0;
-	}
-  	~Set() {
-	}
-  	int count() {
-  	  int count = 0;
-  	  for (int i = 0; i < size; i++)
-  	    if(set[i].isSuperPrime())
-  	      count += 1;
-	  return count; 
-	}
-  	int sum() {
-  	  int sum = 0;
-  	  /*
-  	  for (int i = 0; i < size; i++)
-  	    if(set[i].isSuperPrime())
-  	      sum += set[i];
-  	      */ 
-	  return sum; 
-	}
-  private:
-  	SuperPrime set[1000];
-  	int size;
+  	Prime *psum;
+  	Prime *pmulti;
+  	Prime *psquaresum;
 };
 int main() {
   SuperPrime sp(113);
-  if (sp.isSuperPrime())
+  if (sp.isPrime())
     std::cout << "113 is SuperPrime" << std::endl;
   else
     std::cout << "113 is NOT SuperPrime" << std::endl;
