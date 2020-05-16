45fa3042b3ea8d47839ac329e0fceb9541a5cea6
Cuijiaqi <55613873+Rachel-Scu@users.noreply.github.com>
2020-05-16 10:57:17 +0800

    Revison to last week's homework after Friday class

--- SuperPrime2.cpp
+++ SuperPrime2.cpp
@@ -9,47 +9,75 @@ class Prime {
 	~Prime() {
 	}
   	bool isPrime() { 
-  	  //2到number-1的因子 
-  	  for(int i=2;i<number;i++){
-  	  	if(number%i==0)
-  	  		return false;
-	  } 
+  	  //2到number-1的因子
+	  for(int i=2;i<number;i++){
+	  	if(number%i==0)
+	  		return false;
+	  }	 
   	  return true;
 	}
   private:
   	const int number;
 }; 
-
+class PrimeSet : public Prime {
+  public:
+  	PrimeSet(int size) {
+  	  //集合的构造什么？ 
+  	  set = new Prime*[size];
+  	  this->size = size;
+  	  index = 0;
+	}
+	~PrimeSet() {
+  	  for (int i = 0; i < index; ++i)  //销毁对象 
+		delete set[i]; 
+	  delete[] set;
+	}
+// 	int count() {
+//  	  int count = 0;
+//  	  for (int i = 0; i < size; i++)
+//  	    if(set[i]->isPrime())
+//  	      count += 1;
+//	  return count; 
+//	}
+	bool add(int n) {
+	  if(index == size)  return false;
+	  Prime *p = new Prime(n);
+	  set[index] = p;
+	  index += 1;
+	  return true;
+	}
+	bool isAllPrime() {
+	  for(int i = 0; i < index; i++)
+	    if (!set[i]->isPrime())
+	      return false;
+	  return true;
+	} 
+  private:
+  	Prime **set;
+	int size, index;
+};
 class SuperPrime : public Prime {
   public:
-  	SuperPrime():Prime(0){  
+  	SuperPrime():Prime(0), pset(3) {  //为什么必须有？ 
   	}
-  	SuperPrime(int n):Prime(n){
+  	SuperPrime(int n):Prime(n), pset(3) {
 	  // number split into N
 	  int temp = n;
-	  int tsum=0,tmulti=1,tsquaresum=0;
 	  while(temp > 0) {
 	  	int t = temp % 10;
 	  	temp /= 10;
-	  	tsum+=t;
-		tmulti*=t;
-		tsquaresum+=(t*t);
-		psum=new Prime(tsum);
-		pmulti=new Prime(tmulti);
-		psquaresum=new Prime(tsquaresum);   //作业：单个数字为对象？还是和/积/平方和为对象？ 
+	  	pset.add(t);  //作业：单个数字为对象？还是和/积/平方和为对象？ 
 	  } 
 	}
   	~SuperPrime() {
 	}
   	bool isPrime() {   //类/对象的接口，更抽象说是外观 
-	  if (Prime::isPrime() && psum->isPrime() && pmulti->isPrime() && psquaresum->isPrime())
+	  if (Prime::isPrime() && pset.isAllPrime())
 	    return true; 
   	  return false;
 	}
   private:
-  	Prime *psum;
-  	Prime *pmulti;
-  	Prime *psquaresum;
+  	PrimeSet pset;
 };
 int main() {
   SuperPrime sp(113);
